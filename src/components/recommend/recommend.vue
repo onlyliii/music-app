10c14dc5417793f28a0db8b310992a12b6587127
onlyliii <chncn8@163.com>
2018-03-05 23:05:01 +0800

    添加滚动组件

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,32 +1,46 @@
 <template>
   <div class="recommend" ref="recommend">
-    <div class="recommend-content">
-      <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
-        <Slider>
-          <div v-for="item in recommends">
-            <a :href="item.linkUrl">
-              <img :src="item.picUrl" alt="图片">
-            </a>
-          </div>
-        </Slider>
+    <Scroll ref="scroll" class="recommend-content" :data="discList">
+      <div>
+        <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+          <Slider>
+            <div v-for="item in recommends" :key="item.picUrl">
+              <a :href="item.linkUrl">
+                <img @load="loadImage" :src="item.picUrl" alt="图片">
+              </a>
+            </div>
+          </Slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in discList" :key="item.dissid" class="item">
+              <div class="icon">
+                <img :src="item.imgurl" width="60" height="60" alt="">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul></ul>
-      </div>
-    </div>
+    </Scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import Slider from 'base/slider/slider'
+  import Scroll from 'base/scroll/scroll'
   import {getRecommendJsonp, getDiscList} from 'api/recommend'
   import {ERR_OK} from 'api/config'
 
   export default {
     data: function () {
       return {
-        recommends: []
+        recommends: [],
+        discList: []
       }
     },
     created() {
@@ -38,20 +52,28 @@
         getRecommendJsonp().then((res) => {
           if (res.code === ERR_OK) {
             this.recommends = res.data.slider
-            // 测试 git key 使用
           }
         })
       },
       _getDiscList() {
         getDiscList().then((res) => {
-          console.log(res)
           if (res.code === ERR_OK) {
+            this.discList = res.data.list
           }
         })
+      },
+      // 确保图片加载后better-sccroll刷新
+      loadImage() {
+        if (!this.checkLoaded) {
+          // 避免重复刷新
+          this.$refs.scroll.refresh()
+          this.checkLoaded = true
+        }
       }
     },
     components: {
-      Slider
+      Slider,
+      Scroll
     }
   }
 </script>
